bdfae5bb720657ada625a2a157d436bdb6c5d6cf
Syed Rizvi <asad5688@gmail.com>
2021-05-30 18:16:53 -0500

    fixed navbar spacing on small devices

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
         <Link className="Link" to="/about" >About</Link>
         <Link className="Link" to="/projects" >Projects</Link>
         {/* <Link className="Link" to="/research" >Research</Link> */}
-        <a className="Link" href="./Rizvi_Syed_Resume.pdf" target="_blank">Resume</a>
+        <a className="ALink" href="./Rizvi_Syed_Resume.pdf" target="_blank">Resume</a>
       </ul>
     </div>
   )
