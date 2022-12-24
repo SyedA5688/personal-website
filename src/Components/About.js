9d56ed4d33dc517982a37784fa36bd3ffa8195aa
Syed Rizvi <syedrizvi@Syeds-MacBook-Pro.local>
2022-12-23 23:11:13 -0500

    updated research project section 12-23-2022

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,8 +20,9 @@ export default function About() {
       <Grid item xs={12} lg={8} className="AboutColumn" >
         <h1>About Me</h1>
         <p>
-          I am a final year Computer Science undergraduate at the University of Houston,
-          and a visiting research student at Yale University supervised by <a href="https://medicine.yale.edu/profile/david_vandijk/" target="_blank" rel="noreferrer" >Dr. David van Dijk</a>. 
+          I am a visiting research student at Yale University supervised by 
+          <a href="https://medicine.yale.edu/profile/david_vandijk/" target="_blank" 
+          rel="noreferrer" >Dr. David van Dijk</a>. 
           I'm interested in applying Machine Learning and Graph Neural Networks (GNNs)
           to real-world scientific problems. I am particularly interested in developing
           explainable AI (XAI) systems to bring more interpretability into deep
--- src/Components/Research.js
+++ src/Components/Research.js
@@ -7,6 +7,8 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import morphset_diagram_img from '../images/research/MorphSet_diagram.png';
 import hdgan_diagram_img from '../images/research/hdgan_fig1.png';
+import dcsage_diagram_img from '../images/research/DCSAGE_Architecture_draft6.png';
+import ampnet_diagram_img from '../images/research/ampnet_operator_diagram.png';
 
 export default function Research() {
   return (
@@ -58,17 +60,80 @@ export default function Research() {
 
           <Typography variant="body2" color="text.secondary">
             Semi-supervised frameworks aim to reduce the burden of obtaining expensive 
-            labels on medical image datasets, however existing frameworks struggle to 
-            scale to high-resolution medical images and scarce datasets. In this paper, 
+            labels on medical imaging datasets. Existing frameworks, however, struggle to 
+            scale to high-resolution medical images datasets. In this paper, 
             we extend upon the DatasetGAN semi-supervised image generation 
             and segmentation framework, making improvements to the generative backbone 
-            and computational requirements of the framework which allow the method to 
-            scale better to high-resolution, scarce medical data. 
+            and latent feature usage of the framework. We propose Histopathology 
+            DatasetGAN, a more computationally efficient framework scalable to medical 
+            image datasets. 
           </Typography>
         </CardContent>
         <CardActions>
           <a href="https://arxiv.org/pdf/2207.02712.pdf" target="_blank" rel="noreferrer" ><Button size="Small" >Paper</Button></a>
           <a href="https://github.com/SyedA5688/HistopathologyDatasetGAN" target="_blank" rel="noreferrer" ><Button size="Small" >Code</Button></a>
+        </CardActions>
+      </Card>
+
+      <Card sx={{ maxWidth: 345, mr: 2, ml: 2 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            DCSAGE: Deep Learning-Derived Optimal Aviation Strategies to Control Pandemics
+          </Typography>
+          
+          <CardMedia
+            component="img"
+            height="140"
+            image={dcsage_diagram_img}
+            alt="DCSAGE architecture diagram"
+            style={{ marginTop: '5px', marginBottom: '5px' }}
+          />
+
+          <Typography variant="body2" color="text.secondary">
+            The COVID-19 pandemic has affected countries around the world, motivating 
+            tools for forecasting infection trends and the effects of governmental 
+            interventions. In this work, we study the effect of international flights 
+            on global pandemic dynamics. We propose DCSAGE, a spatiotemporal Graph 
+            Neural Network architecture well-suited for dynamically changing 
+            edge features. We also do sensitivity analysis to study the influence of 
+            flights on infection trends, and explore possible flight reduction 
+            strategies for controlling the pandemic.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <a href="https://arxiv.org/pdf/2210.10888.pdf" target="_blank" rel="noreferrer" ><Button size="Small" >Paper</Button></a>
+          {/* <a href="" target="_blank" rel="noreferrer" ><Button size="Small" >Code</Button></a> */}
+        </CardActions>
+      </Card>
+
+      <Card sx={{ maxWidth: 345, mr: 2, ml: 2 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            AMPNet: Attention as Message Passing for Graph Neural Networks
+          </Typography>
+          
+          <CardMedia
+            component="img"
+            height="140"
+            image={ampnet_diagram_img}
+            alt="AMPNet architecture diagram"
+            style={{ marginTop: '5px', marginBottom: '5px' }}
+          />
+
+          <Typography variant="body2" color="text.secondary">
+            Feature-level interactions carry important information for understanding 
+            interactions between different nodes in network-structured data. Current 
+            explainability frameworks, however, are limited in their ability to uncover
+            feature-level interactions across nodes. In this work, we propose AMPNet, 
+            a general Graph Neural Network message-passing framework for contextualizing
+            feature-level interactions between nodes. We evaluate AMPNet on several 
+            benchmark and real-world datasets, and test its ability to find meaningful
+            feature-level interactions.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <a href="https://arxiv.org/pdf/2210.09475.pdf" target="_blank" rel="noreferrer" ><Button size="Small" >Paper</Button></a>
+          {/* <a href="" target="_blank" rel="noreferrer" ><Button size="Small" >Code</Button></a> */}
         </CardActions>
       </Card>
     </div>
